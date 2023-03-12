f1176a8c2d97dc720c66f7eb13e228b940ec9fb7
Martin Reta <tinchoreta@gmail.com>
2023-03-12 18:02:40 -0300

    animaciones en botón preguntas y logo en 2100px

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,15 +1,55 @@
-const observer = new IntersectionObserver((entries) => {
+const appearObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
        
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add( 'show' );
             
         }
         else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove( 'show' );
         }
     }); 
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => appearObserver.observe(el));
+
+
+// const glowObserver = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+       
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add( 'glow' );
+            
+//         }
+//         else {
+//             entry.target.classList.remove( 'glow' );
+//         }
+//     }); 
+// });
+
+// const glowElement = document.querySelectorAll('.glow');
+// glowElement.forEach((el) => glowObserver.observe(el));
+
+
+const button = document.getElementById('my-button');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+
+            button.classList.add('highlight');
+
+
+            setTimeout(() => {
+                
+                button.classList.remove('highlight');
+            }, 3000);
+        } else {
+            
+            button.classList.remove('highlight');
+        }
+    });
+});
+
+observer.observe(button);
